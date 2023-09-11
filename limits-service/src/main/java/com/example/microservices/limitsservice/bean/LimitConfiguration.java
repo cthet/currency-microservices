e724b56eef4a29296e0b110a050bdafb6ee4e637
cthet <clement.thet@gmail.com>
2023-09-11 23:06:41 +0200

    first commit

---
+++ limits-service/src/main/java/com/example/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+    private int maximum;
+    private int minimum;
+
+    protected LimitConfiguration(){}
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public LimitConfiguration(int maximum, int minimum) {
+        super();
+        this.maximum = maximum;
+        this.minimum = minimum;
+    }
+}
